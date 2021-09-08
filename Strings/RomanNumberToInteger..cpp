589acf4ca420eced7606b08672538d2174819da7
Sidhartha Parasramka <52444196+sidhartha8011@users.noreply.github.com>
2021-09-08 15:10:57 +0530

    Create RomanNumberToInteger..cpp

---
+++ Strings/RomanNumberToInteger..cpp
@@ -0,0 +1,63 @@
+// { Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution {
+  public:
+  int value(char r)
+{
+    if (r == 'I')
+        return 1;
+    if (r == 'V')
+        return 5;
+    if (r == 'X')
+        return 10;
+    if (r == 'L')
+        return 50;
+    if (r == 'C')
+        return 100;
+    if (r == 'D')
+        return 500;
+    if (r == 'M')
+        return 1000;
+ 
+    return -1;
+}
+    int romanToDecimal(string &str) {
+        // code here
+        int ans=0;
+        int num1,num2;
+        for(int i=0;i<str.length();i++){
+            num1=value(str[i]);
+            num2=value(str[i+1]);
+            
+            if(num1<num2){
+                ans=ans-num1;
+            }
+            else{
+                ans=ans+num1;
+            }
+            
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        Solution ob;
+        cout << ob.romanToDecimal(s) << endl;
+    }
+}  // } Driver Code Ends
